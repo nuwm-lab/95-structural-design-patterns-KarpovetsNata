3cc56711e313d2d7c5ddc44e7295cf3a7834c2ee
Карповець Наталія <karpovets_ak24@nuwm.edu.ua>
2025-11-21 02:18:10 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,18 +1,93 @@
-﻿using System;
+using System;
 
-namespace LabWork
+// --- Цільовий інтерфейс (Target) ---
+interface IJoystick
 {
-    // Даний проект є шаблоном для виконання лабораторних робіт
-    // з курсу "Об'єктно-орієнтоване програмування та патерни проектування"
-    // Необхідно змінювати і дописувати код лише в цьому проекті
-    // Відео-інструкції щодо роботи з github можна переглянути 
-    // за посиланням https://www.youtube.com/@ViktorZhukovskyy/videos 
-    class Program
+    void MoveUp();
+    void MoveDown();
+    void MoveLeft();
+    void MoveRight();
+    void Fire();
+}
+
+// --- Старий джойстик (Adaptee) ---
+class OldJoystick
+{
+    public void Up() => Console.WriteLine("OldJoystick: Up");
+    public void Down() => Console.WriteLine("OldJoystick: Down");
+    public void Left() => Console.WriteLine("OldJoystick: Left");
+    public void Right() => Console.WriteLine("OldJoystick: Right");
+    public void Shoot() => Console.WriteLine("OldJoystick: Shoot");
+}
+
+// --- Новий джойстик (Adaptee) ---
+class NewJoystick
+{
+    public void MoveVertical(int delta)
     {
-        static void Main(string[] args)
-        {
-            
-            Console.WriteLine("Hello World!");
-        }
+        if (delta > 0) Console.WriteLine("NewJoystick: Move Up");
+        else if (delta < 0) Console.WriteLine("NewJoystick: Move Down");
+    }
+
+    public void MoveHorizontal(int delta)
+    {
+        if (delta > 0) Console.WriteLine("NewJoystick: Move Right");
+        else if (delta < 0) Console.WriteLine("NewJoystick: Move Left");
+    }
+
+    public void FireButton() => Console.WriteLine("NewJoystick: Fire");
+}
+
+// --- Адаптер для старого джойстика ---
+class OldJoystickAdapter : IJoystick
+{
+    private OldJoystick _oldJoystick;
+
+    public OldJoystickAdapter(OldJoystick oldJoystick)
+    {
+        _oldJoystick = oldJoystick;
+    }
+
+    public void MoveUp() => _oldJoystick.Up();
+    public void MoveDown() => _oldJoystick.Down();
+    public void MoveLeft() => _oldJoystick.Left();
+    public void MoveRight() => _oldJoystick.Right();
+    public void Fire() => _oldJoystick.Shoot();
+}
+
+// --- Адаптер для нового джойстика ---
+class NewJoystickAdapter : IJoystick
+{
+    private NewJoystick _newJoystick;
+
+    public NewJoystickAdapter(NewJoystick newJoystick)
+    {
+        _newJoystick = newJoystick;
+    }
+
+    public void MoveUp() => _newJoystick.MoveVertical(1);
+    public void MoveDown() => _newJoystick.MoveVertical(-1);
+    public void MoveLeft() => _newJoystick.MoveHorizontal(-1);
+    public void MoveRight() => _newJoystick.MoveHorizontal(1);
+    public void Fire() => _newJoystick.FireButton();
+}
+
+// --- Клієнт ---
+class Program
+{
+    static void Main()
+    {
+        IJoystick oldJs = new OldJoystickAdapter(new OldJoystick());
+        IJoystick newJs = new NewJoystickAdapter(new NewJoystick());
+
+        Console.WriteLine("Використання старого джойстика через адаптер:");
+        oldJs.MoveUp();
+        oldJs.MoveRight();
+        oldJs.Fire();
+
+        Console.WriteLine("\nВикористання нового джойстика через адаптер:");
+        newJs.MoveDown();
+        newJs.MoveLeft();
+        newJs.Fire();
     }
 }
